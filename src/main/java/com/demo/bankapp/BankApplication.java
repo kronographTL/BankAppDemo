6274c18e5dc99d1424d41bd1ebc8a5b1f6aa3bbb
kronographTL <kronographtl@gmail.com>
2020-07-01 12:23:23 +0800

    Restructuring the project

--- src/main/java/com/demo/bankapp/BankApplication.java
+++ src/main/java/com/demo/bankapp/BankApplication.java
@@ -1,4 +1,4 @@
-package com.demo.bankapp.bankappdemo;
+package com.demo.bankapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
